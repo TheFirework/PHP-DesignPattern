bfd002b9dde8586c6d2c6e964bb395abd937f98c
Fireworks <jiangbo1994925@gmail.com>
2020-05-05 20:57:10 +0800

    PHP设计模式之装饰器模式

--- DecoratorMode/decorator.php
+++ DecoratorMode/decorator.php
@@ -1,1 +1,81 @@
 <?php
+
+interface Shape
+{
+    public function draw();
+}
+
+/**
+ * 圆形实现类
+ * Class Circle
+ */
+class Circle implements Shape
+{
+    public function draw()
+    {
+        print_r("Shape: Circle" );
+    }
+}
+
+/**
+ * 矩形实现类
+ * Class Rectangle
+ */
+class Rectangle implements Shape
+{
+    public function draw()
+    {
+        print_r("Shape: Rectangle");
+    }
+}
+
+/**
+ * 形状装饰类
+ * Class ShapeDecorator
+ */
+abstract class ShapeDecorator implements Shape
+{
+    protected $decoratorShape;
+
+    public function __construct(Shape $shape)
+    {
+        $this->decoratorShape = $shape;
+    }
+
+    public function draw()
+    {
+        $this->decoratorShape->draw();
+    }
+}
+
+
+class RedShapeDecorator extends ShapeDecorator{
+
+    public function __construct(Shape $shape)
+    {
+        parent::__construct($shape);
+    }
+
+    public function draw()
+    {
+        $this->decoratorShape->draw();
+        $this->setRedColor($this->decoratorShape);
+    }
+
+    private function setRedColor(Shape $shape)
+    {
+        print_r("red");
+    }
+}
+
+
+$circle = new Circle();
+$rectangle = new Rectangle();
+
+$redCircle = new RedShapeDecorator($circle);
+$redCircle->draw();
+
+echo '<br/>';
+
+$redRectangle = new RedShapeDecorator($rectangle);
+$redRectangle->draw();
