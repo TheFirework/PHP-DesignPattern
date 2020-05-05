1a290ef7c15ec09d60d40c0a16d0f691d813cb1e
Fireworks <jiangbo1994925@gmail.com>
2020-05-05 21:29:09 +0800

    PHP设计模式之代理模式

---
+++ ProxyMode/proxy.php
@@ -0,0 +1,62 @@
+<?php
+
+//定义RealSubject和Proxy共同具备的东西
+interface Subject
+{
+    function say();
+
+    function run();
+}
+
+class RealSubject implements Subject
+{
+    private $name;
+
+    function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    function say()
+    {
+        echo $this->name . "在吃饭<br>";
+    }
+
+    function run()
+    {
+        echo $this->name . "在跑步<br>";
+    }
+}
+
+class Proxy implements Subject
+{
+    private $realSubject = null;
+
+    function __construct(RealSubject $realSubject = null)
+    {
+        if (empty($realSubject)) {
+            $this->realSubject = new RealSubject();
+        } else {
+            $this->realSubject = $realSubject;
+        }
+    }
+
+    function say()
+    {
+        $this->realSubject->say();
+    }
+
+    function run()
+    {
+        $this->realSubject->run();
+    }
+}
+
+
+$subject = new RealSubject("张三");
+$subject->say();
+$subject->run();
+
+$proxy = new Proxy($subject);
+$proxy->say();
+$proxy->run();
